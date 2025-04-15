64c6fc53b5d39c1f0c4bf7e0ad181035ca06b644
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2025-04-15 10:00:46 -0500

    Remove Windows Server configuration

--- scripts/PowerSTIG/0.0.2/Windows/Windows.ps1
+++ scripts/PowerSTIG/0.0.2/Windows/Windows.ps1
@@ -6,7 +6,7 @@ configuration Windows
     [scriptblock]$localConfigurationManager = {
         LocalConfigurationManager {
             ActionAfterReboot  = 'ContinueConfiguration'
-            ConfigurationMode  = 'ApplyOnly'
+            ConfigurationMode  = 'ApplyOnly' # TODO: Change to ApplyAndAutoCorrect?
             RebootNodeIfNeeded = $true
         }
     }
@@ -36,7 +36,7 @@ configuration Windows
     [scriptblock]$windowsFirewallStig = {
 
         WindowsFirewall STIG_WindowsFirewall {
-            Skiprule = @('V-242005', 'V-242004')
+            SkipRule = @('V-242005', 'V-242004')
         }
     }
 
@@ -84,7 +84,7 @@ configuration Windows
                 Reset_account_lockout_counter_after = 15
             }
         }
-        else if ($osVersion -match "Windows 11") {
+        elseif ($osVersion -match "Windows 11") {
             WindowsClient STIG_WindowsClient {
                 OsVersion   = '11'
                 SkipRule    = @("V-220740", "V-220739", "V-220741", "V-220908")
@@ -115,79 +115,16 @@ configuration Windows
                 Reset_account_lockout_counter_after = 15
             }
         }
-
-        switch -Wildcard ($osVersion) {
-            "*2016*" {
-                $osVersion = '2016'
-                $skipRules = @('V-224866', 'V-224867', 'V-224868')
-                $exceptions = @{
-                    'V-225019' = @{Identity = 'Guests' }
-                    'V-225016' = @{Identity = 'Guests' }
-                    'V-225018' = @{Identity = 'Guests' }
-                }
-                $orgSettings = @{
-                    'V-225015' = @{Identity = 'Guests' }
-                    'V-225027' = @{OptionValue = 'xGuest' }
-                    'V-225063' = @{ValueData = '2' }
-                }
-
-                WindowsServer STIG_WindowsServer {
-                    OsVersion   = $osVersion
-                    OsRole      = 'MS'
-                    Exception   = $exceptions
-                    OrgSettings = $orgSettings
-                    SkipRule    = $skipRules
-                }
-
-                AccountPolicy BaseLine2 {
-                    Name                                = "2016fix"
-                    Account_lockout_threshold           = 3
-                    Account_lockout_duration            = 15
-                    Reset_account_lockout_counter_after = 15
-                }
-                break
-            }
-            "*2019*" {
-                $osVersion = '2019'
-                $skipRules = @('V-205629', 'V-205630', 'V-205795')
-                $exceptions = @{
-                    'V-205733' = @{Identity = 'Guests' }
-                    'V-205672' = @{Identity = 'Guests' }
-                    'V-205673' = @{Identity = 'Guests' }
-                    'V-205675' = @{Identity = 'Guests' }
-                }
-                $orgSettings = @{
-                    'V-205910' = @{OptionValue = 'xGuest' }
-                    'V-205717' = @{ValueData = '2' }
-                }
-
-                WindowsServer STIG_WindowsServer {
-                    OsVersion   = $osVersion
-                    OsRole      = 'MS'
-                    Exception   = $exceptions
-                    OrgSettings = $orgSettings
-                    SkipRule    = $skipRules
-                }
-
-                AccountPolicy BaseLine2 {
-                    Name                                = "2019fix"
-                    Account_lockout_threshold           = 3
-                    Account_lockout_duration            = 15
-                    Reset_account_lockout_counter_after = 15
-                }
-                break
-            }
-        }
     }
-
-    Node localhost
-    {
-        $localConfigurationManager.invoke()
-        $windowsStig.invoke()
-        $ie11Stig.invoke()
-        $dotnetFrameworkStig.invoke()
-        $windowsDefenderStig.invoke()
-        $windowsFirewallStig.invoke()
-        $microsoftEdgeStig.invoke()
-    }
+    
+	Node localhost
+	{
+		$localConfigurationManager.Invoke()
+		$windowsStig.Invoke()
+		$ie11Stig.Invoke()
+		$dotnetFrameworkStig.Invoke()
+		$windowsDefenderStig.Invoke()
+		$windowsFirewallStig.Invoke()
+		$microsoftEdgeStig.Invoke()
+	}
 }
