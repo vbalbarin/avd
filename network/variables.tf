cfbd71a03c16e3d071892d21f8e244c1cf710993
vbalbarin <vbalbarin@microsoft.com>
2025-05-13 09:30:43 -0400

    Removing unused variables

--- network/variables.tf
+++ network/variables.tf
@@ -31,39 +31,10 @@ variable "env" {
   }
 }
 
-variable "vnet" {
-  default = ""
-  type    = string
-}
 
 variable "vnet_address_space" {
   default = "10.1.0.0/23"
   type    = string
-}
-
-variable "vnet_rg" {
-  default = ""
-  type    = string
-}
-
-variable "snet" {
-  default = ""
-  type    = string
-}
-
-variable "rdsh_count" {
-  default = 2
-  type    = number
-}
-
-variable "encryption_at_host_enabled" {
-  default = false
-  type    = bool
-}
-
-variable "enroll_in_intune" {
-  default = true
-  type    = bool
 }
 
 variable "az_region_abbreviations" {
